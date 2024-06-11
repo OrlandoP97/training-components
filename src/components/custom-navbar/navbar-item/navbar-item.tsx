fbdd4b1e5e997d0336ba6ce33bb7e42b825bb511
Orlando <57908549+OrlandoP97@users.noreply.github.com>
2024-06-11 14:47:33 -0300

    first commit

---
+++ src/components/custom-navbar/navbar-item/navbar-item.tsx
@@ -0,0 +1,15 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'navbar-item',
+  styleUrl: 'navbar-item.css',
+  shadow: true,
+})
+export class NavbarItem {
+  
+  
+
+  render() {
+    return <div>Hello, World!</div>;
+  }
+}
---
+++ src/components/custom-navbar/navbar/navbar.tsx
@@ -0,0 +1,27 @@
+import { Component, Prop, Event, EventEmitter, h } from '@stencil/core';
+
+@Component({
+  tag: 'navbar',
+  styleUrl: 'navbar.css',
+  shadow: true,
+})
+export class Navbar {
+  @Prop() brand: string;
+
+  //Los links deben ser los navBar-items
+
+  @Prop() fixed: boolean = false;
+  @Prop() transparent: boolean = false;
+
+
+  render() {
+    return (
+      <div class={`navbar ${this.fixed ? 'fixed' : ''} ${this.transparent ? 'transparent' : ''}`}>
+        <div class="brand">{this.brand}</div>
+        <div class="links">
+         <slot></slot>
+        </div>
+      </div>
+    );
+  }
+}
